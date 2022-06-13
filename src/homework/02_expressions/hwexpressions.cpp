d7411055b178207b8bab2ba6183c2df2a4350289
JelenaDjukic1 <jelenadjukic97@yahoo.com>
2022-06-13 22:09:20 +0000

    Homework2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,12 +1,18 @@
-#include "expressions.h"
+#include "hwexpressions.h"
+#include <iostream>
+using namespace std;
 
-//example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
+const double tax_rate = 6.75;
 
-//write function code here
+	double get_sales_tax_amount(double meal_amount)
+	{
+		return meal_amount*(tax_rate/100);
+	}
+	double get_tip_amount(double meal_amount,double tip_rate)
+	{
+		return meal_amount*(tip_rate/100);
+	}
+
 
 
 
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -2,13 +2,16 @@
 #include "catch.hpp"
 #include "hwexpressions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
 
-TEST_CASE("Verify sum_numbers function") 
+
+TEST_CASE("Test get_sales_tax")
 {
-	REQUIRE(add_numbers(5, 5) == 10);
-	REQUIRE(add_numbers(10, 10) == 20);
+	REQUIRE(get_sales_tax_amount(10) == 0.675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
 }
 
+TEST_CASE("Test get_tip_amount")
+{
+	REQUIRE(get_tip_amount(20,15) == 3);
+	REQUIRE(get_tip_amount(20,20) == 4);
+}
