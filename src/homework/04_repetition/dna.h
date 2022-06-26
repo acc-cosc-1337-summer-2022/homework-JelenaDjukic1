23de8e4fe4334390fced703e245a2636e955f92a
JelenaDjukic1 <jelenadjukic97@yahoo.com>
2022-06-26 03:22:50 +0000

    Homework 4

--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,5 +1,8 @@
-//add include statements
 
-/*
-Write prototype for function(s)
-*/
+#define DNA_H
+#include<string>
+using namespace std;
+
+double get_gc_content(const string& dna);
+string get_reverse_string(string dna);
+string get_dna_complement(string dna);
